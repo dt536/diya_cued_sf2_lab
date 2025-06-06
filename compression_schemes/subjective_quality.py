f599fc164a1ad47c1853da67ed38dd72c86445a2
Diya Thomas <123311674+dt536@users.noreply.github.com>
2025-06-06 20:12:25 +0000

    adding vif

--- compression_schemes/subjective_quality.py
+++ compression_schemes/subjective_quality.py
@@ -1,5 +1,5 @@
 from skimage.metrics import structural_similarity
-
+import sewar
 # Structural Similarity Index (SSIM)
 def ssim(A, B):
     """Compare A and B for visual similarity
@@ -8,3 +8,7 @@ def ssim(A, B):
     data_range = max(A.max(), B.max()) - min(A.min(), B.min())
     struct_sim, _ = structural_similarity(A, B, data_range=data_range, full=True)
     return struct_sim
+
+#Visual Information Fidelity
+def vif_index(imageA, imageB):
+    return sewar.full_ref.vifp(imageA, imageB)
